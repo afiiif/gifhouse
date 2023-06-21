39c952f3911782bf334d139b0ebb84823a42ab6c
M. Afifudin <afif.x.udin@gmail.com>
2023-06-22 06:26:38 +0700

    feat: protect favorites & account route

--- src/app/(auth)/account/page.tsx
+++ src/app/(auth)/account/page.tsx
@@ -1,11 +1,25 @@
 'use client';
 
+import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 
+import { useAuthStore } from '@/stores/auth';
 import { signOut } from '@/utils/firebase';
 
 export default function AccountPage() {
   const router = useRouter();
+
+  const { isLoading, user } = useAuthStore();
+
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.replace('/');
+    }
+  }, [isLoading, user, router.replace]);
+
+  if (!user) {
+    return 'Checking your account...';
+  }
 
   return (
     <>
--- src/app/(public)/favorites/page.tsx
+++ src/app/(public)/favorites/page.tsx
@@ -1,6 +1,33 @@
+'use client';
+
+import Link from 'next/link';
+
+import { useAuthStore } from '@/stores/auth';
+
 import GifGrid from './GifGrid';
 
 export default function FavoritesPage() {
+  const { isLoading, user } = useAuthStore();
+
+  if (isLoading) {
+    return 'Checking your account...';
+  }
+
+  if (!user) {
+    return (
+      <>
+        <h1 className="pb-6 text-2xl font-bold">My Favorites</h1>
+        <p className="pb-8">Please log in to your account before adding items to your favorites.</p>
+        <Link
+          href="/login"
+          className="rounded bg-fuchsia-600 px-6 py-2 text-white hover:bg-fuchsia-500 focus:bg-fuchsia-700"
+        >
+          Login
+        </Link>
+      </>
+    );
+  }
+
   return (
     <>
       <h1 className="pb-6 text-2xl font-bold">My Favorites</h1>
